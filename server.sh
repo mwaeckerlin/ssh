0abd3b3d07a05822f7af95eac88a7190824a6752
Marc Wäckerlin <marc@waeckerlin.org>
2016-06-25 15:39:43 +0200

    separate several keys with backslash+«n» \n

--- server.sh
+++ server.sh
@@ -3,9 +3,7 @@
 /etc/init.d/nscd start
 if test -n "${SSHKEY}"; then
     test -d ~/.ssh || mkdir ~/.ssh
-    cat > ~/.ssh/authorized_keys <<EOF
-${SSHKEY}
-EOF
+    echo -e "${SSHKEY}"  > ~/.ssh/authorized_keys
 fi
 echo "setup ready, starting ssh daemon ..."
 /usr/sbin/sshd $SSHOPTIONS -D
